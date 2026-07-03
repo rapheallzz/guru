998433b05741d923d2edde49a9ec5c316ae59fcc
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2026-07-03 13:55:26 +0100

    Update FeaturedProducts.tsx

--- src/components/home/FeaturedProducts.tsx
+++ src/components/home/FeaturedProducts.tsx
@@ -8,7 +8,7 @@ export function FeaturedProducts() {
   const featured = products.filter(p => p.featured);
 
   return (
-    <section className="relative z-20 -mt-24 pb-32">
+    <section className="relative z-20 -mt-8 pb-32">
       <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
         <ProductSlider products={featured} />
       </div>
